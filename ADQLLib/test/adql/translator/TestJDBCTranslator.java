35886468214498013edbbc8ff01cb73231f4d723
theresa dower <dower@stsci.edu>
2026-04-30 12:12:44 -0400

    add negative operands parens fix and test

--- ADQLLib/test/adql/translator/TestJDBCTranslator.java
+++ ADQLLib/test/adql/translator/TestJDBCTranslator.java
@@ -3,6 +3,7 @@ package adql.translator;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNull;
 import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.fail;
 
 import java.util.ArrayList;
@@ -58,6 +59,25 @@ public class TestJDBCTranslator {
 	@Before
 	public void setUp() throws Exception {
 	}
+
+	@Test
+    public void testExtraParenOperandsMain21() {
+        // MAST TAP based on ADQL 2.1 branch is incorrectly applying parens in arithmetic statements with >2 items.
+        // This is causing incorrect results for negative values. Testing where this fails in upstream branches.
+        try {
+			ADQLParser parser = new ADQLParser(ADQLVersion.V2_1);
+            JDBCTranslator translator = new AJDBCTranslator();
+            ADQLSet query = parser.parseQuery("SELECT (FOO-1-2) FROM BAR");
+            System.out.println(translator.translate(query));
+            assertFalse(translator.translate(query).contains("((FOO-(1-2))"));
+        } catch(ParseException pe) {
+            pe.printStackTrace();
+            fail("The given ADQL query is completely correct. No error should have occurred while parsing it. (see the console for more details)");
+        } catch (TranslationException te) {
+            te.printStackTrace();
+            fail("No error was expected from this translation. (see the console for more details)");
+        }
+    }
 
 	@Test
 	public void testTranslateSetOperation() {
