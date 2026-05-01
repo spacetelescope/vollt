30e5bae81bbbd97b86b1d0b134815b0099c03774
theresa dower <dower@stsci.edu>
2026-05-01 10:32:26 -0400

    remove unused import

--- ADQLLib/test/adql/translator/TestJDBCTranslator.java
+++ ADQLLib/test/adql/translator/TestJDBCTranslator.java
@@ -3,7 +3,6 @@ package adql.translator;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNull;
 import static org.junit.Assert.assertTrue;
-import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.fail;
 
 import java.util.ArrayList;
